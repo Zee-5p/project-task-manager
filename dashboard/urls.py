ee4acbc840764a9d57ec0fcb53c252b1ddff521e
Zeeshan Mahmood <izosscfe@students.codeinstitute.net>
2025-07-07 18:20:31 +0100

    Add URL pattern for adding tasks

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('projects/<int:pk>/edit/', views.edit_project, name='edit_project'),
     path('projects/<int:pk>/delete/', views.delete_project, name='delete_project'),
     path('projects/<int:project_pk>/tasks/', views.task_list, name='task_list'),
+    path('projects/<int:project_pk>/tasks/add/', views.add_task, name='add_task'),
 ]
