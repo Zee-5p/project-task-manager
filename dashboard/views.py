b119b69cc890cbc250806d37443de612e6620dcd
Zeeshan Mahmood <izosscfe@students.codeinstitute.net>
2025-07-07 17:14:00 +0100

    Add task list view and template to display tasks per project

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,17 +1,20 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
 from .models import Project
 from .forms import ProjectForm
-from django.contrib.auth.decorators import login_required
+from .models import Task 
 
 @login_required
 def project_list(request):
+    # Show only the projects created by the current user
     projects = Project.objects.filter(created_by=request.user)
     return render(request, 'dashboard/project_list.html', {'projects': projects})
 
 @login_required
 def edit_project(request, pk):
+    # Only allow the user to edit their own project
     project = get_object_or_404(Project, pk=pk, created_by=request.user)
-    
+
     if request.method == 'POST':
         form = ProjectForm(request.POST, instance=project)
         if form.is_valid():
@@ -19,5 +22,21 @@ def edit_project(request, pk):
             return redirect('project_list')
     else:
         form = ProjectForm(instance=project)
-    
+
     return render(request, 'dashboard/edit_project.html', {'form': form, 'project': project})
+
+@login_required
+def delete_project(request, pk):
+    project = get_object_or_404(Project, pk=pk, created_by=request.user)
+    
+    if request.method == 'POST':
+        project.delete()
+        return redirect('project_list')
+    
+    return render(request, 'dashboard/confirm_delete.html', {'project': project})
+
+@login_required
+def task_list(request, project_pk):
+    project = get_object_or_404(Project, pk=project_pk, created_by=request.user)
+    tasks = Task.objects.filter(project=project)
+    return render(request, 'dashboard/task_list.html', {'project': project, 'tasks': tasks})
