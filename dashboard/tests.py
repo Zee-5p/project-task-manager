5a34c2530a4cc68ed99aaad09d64962735d5f909
Zeeshan Mahmood <izosscfe@students.codeinstitute.net>
2025-07-08 14:37:30 +0100

    Added unit tests for Project and Task models and project list view

--- dashboard/tests.py
+++ dashboard/tests.py
@@ -1,3 +1,35 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
+from .models import Project, Task
+from django.urls import reverse
+from datetime import date
 
-# Create your tests here.
+class DashboardTests(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='pass')
+        self.client = Client()
+        self.client.login(username='testuser', password='pass')
+        self.project = Project.objects.create(
+            name='Test Project',
+            description='Description here',
+            created_by=self.user
+        )
+        self.task = Task.objects.create(
+            project=self.project,
+            title='Test Task',
+            description='Do something',
+            assigned_to=self.user,
+            status='TODO',
+            due_date=date.today()
+        )
+
+    def test_project_str(self):
+        self.assertEqual(str(self.project), 'Test Project')
+
+    def test_task_str(self):
+        self.assertEqual(str(self.task), 'Test Task (To Do)')
+
+    def test_project_list_view(self):
+        response = self.client.get(reverse('project_list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Test Project')
