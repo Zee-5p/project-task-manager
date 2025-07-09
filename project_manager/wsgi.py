2e8e3b08a051eb7e4ce4ecd4e77d029d6e1e6a89
Zeeshan Mahmood <izosscfe@students.codeinstitute.net>
2025-07-09 03:42:48 +0100

    migration issue fix

--- project_manager/wsgi.py
+++ project_manager/wsgi.py
@@ -14,3 +14,8 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project_manager.settings')
 
 application = get_wsgi_application()
+
+import django
+django.setup()
+from django.core.management import call_command
+call_command('migrate')
