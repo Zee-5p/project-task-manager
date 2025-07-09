da21d939d7382c41f447cf41657726d4ddd9d937
Zeeshan Mahmood <izosscfe@students.codeinstitute.net>
2025-07-09 03:31:36 +0100

    Ensure migrations applied and update deployment readiness

--- project_manager/settings.py
+++ project_manager/settings.py
@@ -68,7 +68,9 @@ DATABASES = {
     }
 }
 
-DATABASES['default'] = dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+DATABASES['default'] = dj_database_url.config(
+    default=os.environ.get('DATABASE_URL', f'sqlite:///{BASE_DIR / "db.sqlite3"}')
+)
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
