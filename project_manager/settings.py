c990e1ff5c19cb6fc2a3dce83d493d75fea00b03
Zeeshan Mahmood <izosscfe@students.codeinstitute.net>
2025-07-09 01:57:37 +0100

    updates

--- project_manager/settings.py
+++ project_manager/settings.py
@@ -1,22 +1,14 @@
 from pathlib import Path
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'dev-secret-key')
-
-# SECURITY WARNING: don't run with debug turned on in production!
 import os
 import dj_database_url
 
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = os.environ.get('SECRET_KEY', 'dev-secret-key')
+
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['project-manager.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['project_manager.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
